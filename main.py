849366a3c63eda14a79038ed8153893c29a4da6f
Oleh <olehmylyan2006@gmail.com>
2026-01-02 16:22:34 +0100

    Homework Module 3

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello World")
+with open("test.txt", "w") as fh:
+    fh.write("first line\nsecond line\nthird line")
 
-print("Hello Git")
+with open("test.txt", "r") as fh:
+    lines = [el.strip() for el in fh.readlines()]
+
+print(lines)
